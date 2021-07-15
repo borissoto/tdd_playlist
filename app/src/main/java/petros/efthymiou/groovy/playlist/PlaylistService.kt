6242b7527d87cabeddc7a009297dad1b3957dddc
Boris <borissoto@gmail.com>
2021-07-14 23:22:32 -0400

    Completed the inner TDD cycle for the repository layer

---
+++ app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistService.kt
@@ -0,0 +1,9 @@
+package petros.efthymiou.groovy.playlist
+
+import kotlinx.coroutines.flow.Flow
+
+class PlaylistService {
+    fun fetchPlaylists() : Flow<Result<List<Playlist>>> {
+        TODO("Not yet implemented")
+    }
+}
