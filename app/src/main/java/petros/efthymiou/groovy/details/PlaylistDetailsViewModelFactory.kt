344c8336499ea5a6f7c2c291ab936f95b9631c41
Boris <borissoto@gmail.com>
2021-07-28 22:21:03 -0400

    Finished with implementation of the ViewModel and get out of the inner Tdd cycle

--- app/src/main/java/petros/efthymiou/groovy/details/PlaylistDetailsViewModelFactory.kt
+++ app/src/main/java/petros/efthymiou/groovy/details/PlaylistDetailsViewModelFactory.kt
@@ -6,10 +6,10 @@ import petros.efthymiou.groovy.playlist.PlaylistRepository
 import javax.inject.Inject
 
 class PlaylistDetailsViewModelFactory @Inject constructor(
+    private val service : PlaylistDetailsService
 ): ViewModelProvider.Factory {
+
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return PlaylistDetailsViewModel() as T
+        return PlaylistDetailsViewModel(service) as T
     }
-
-
 }
